e741bf81df2eaba908fd9e1aa35ffcb442cb87f3
fotis91 <gfotishij@gmail.com>
2018-11-02 22:56:33 +0200

    Add files via upload
    HLS source code

---
+++ lzw.c
@@ -0,0 +1,102 @@
+//#include <ap_cint.h>
+
+
+struct DictNode {
+	int value; // the position in the list
+	int prefix; // prefix for byte > 255
+	int character; // the last byte of the string
+};
+
+
+void dictionaryInit(struct DictNode nodeinit[1023]);
+
+int dictionaryLookup(int prefix, int character,struct DictNode nodelookup[1023]);
+
+void dictionaryAdd(int prefix, int character, int value,struct DictNode nodeadd[1023]);
+
+void compress(int character,int * WC,int done) {
+
+
+
+	static struct DictNode p_nodes[1023];
+    static int init=0;
+    static int prefix;
+    static int nextCode=1000;
+    static int dictionarySize=2023;
+    static int temp_prefix;
+    static int count_codes=0;
+    int index;
+
+
+    if(init==0){
+    dictionaryInit(p_nodes);
+    init=1;}
+
+    if ((index = dictionaryLookup(prefix,character, p_nodes)) != -1){ prefix = index;
+     if(count_codes==done) *WC=index;
+     else *WC=-1;
+	     }
+    else {count_codes++;
+     if (nextCode < dictionarySize) {
+	dictionaryAdd(prefix,character, nextCode++, p_nodes);}
+
+    temp_prefix=prefix;
+
+    prefix=character;
+
+    if(count_codes==done) *WC=(character<<11)+temp_prefix;
+
+    else *WC=temp_prefix;
+    }
+
+}
+
+void dictionaryInit(struct DictNode nodeinit[1023]) {
+	for (int i = 0; i <= 999; i++) {
+	    	nodeinit[i].prefix=-1;
+	    	nodeinit[i].character=i;
+	        nodeinit[i].value=i;
+	    }
+	    for (int j = 1000; j < 1023; j++) {
+	    	nodeinit[j].prefix=-1;
+	    	nodeinit[j].character=-1;
+	        nodeinit[j].value=-1;
+	    }
+}
+
+int dictionaryLookup(int prefix, int character,struct DictNode nodelookup[1023]) {
+
+	if(prefix!=-1){
+
+	      for (int i = 0; i < 1024; i++) {
+
+	       if(nodelookup[i].value<999) return -1;
+
+	       else{
+	           if(nodelookup[i].prefix == prefix && nodelookup[i].character == character) return nodelookup[i].value;
+	           }
+	       }
+
+	      }
+
+	    else{
+	        for (int i = 0; i < 1000; i++){
+	          if(nodelookup[i].character == character) return nodelookup[i].value;
+	        }
+	    }
+return -1;
+
+}
+
+
+void dictionaryAdd(int prefix, int character, int value,struct DictNode nodeadd[1023]) {
+    static int cnt=0;
+    nodeadd[cnt].value = value;
+    nodeadd[cnt].prefix = prefix;
+    nodeadd[cnt].character = character;
+	cnt++;
+}
+
+
+
+

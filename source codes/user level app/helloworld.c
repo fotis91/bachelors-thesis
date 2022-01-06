31b72b16d88cb964bc33dc7c1aa91fc9426ebaa8
fotis kostarelos <gfotishij@gmail.com>
2022-01-06 18:24:17 +0200

    Update helloworld.c

--- source codes/user level app/helloworld.c
+++ source codes/user level app/helloworld.c
@@ -30,20 +30,6 @@
 *
 ******************************************************************************/
 
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
 
 /* THIS IS A SIMPLE APPLICATION MANAGING THE HARDWARE ACCELERATOR CALLED "compress". IT INITIALIZES THE ACCELERATOR AND FEEDS IT WITH DATA*/
 
